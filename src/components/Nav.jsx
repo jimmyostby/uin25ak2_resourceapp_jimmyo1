5f332e079ba5cf3cc7562a67e14fb74adc7672dc
Jimmy Østby <142933101+jimmyostby@users.noreply.github.com>
2025-03-06 11:11:39 +0100

    Startet med layout

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+export default function Nav() {
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="#"></Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
